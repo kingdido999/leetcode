eefd62eb75770aa26b3fc0244e1cdcc6e0db987f
Pengcheng Ding <info@desmonding.me>
2022-08-16 11:54:53 -0700

    Add linked-list.remove-nth-node-from-end-of-list.ts

---
+++ linked-list.remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+  class ListNode {
+      val: number
+      next: ListNode | null
+  }
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  if (head === null) {
+    return head
+  }
+
+  let dummy = new ListNode()
+  dummy.next = head
+  let l = dummy
+  let r = dummy
+
+  // Make the left and right pointers n nodes apart
+  for (let i = 0; i < n; i++) {
+    r = r.next
+  }
+
+  // Move the left and right pointers to the right until the right pointer
+  // reaches the last node
+  while (r !== null && r.next !== null) {
+    l = l.next
+    r = r.next
+  }
+
+    // Remove the node immediately after the left node, i.e., the Nth node
+  l.next = l.next.next
+
+  return dummy.next
+};
